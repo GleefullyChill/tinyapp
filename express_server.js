090a6f698028a7dd8c81dbaa93ea5227cf0b146e
GledfullyChill <stephan.lofstrom@gmail.com>
2021-08-02 15:25:53 -0600

    put the uurlDatabase on the /urls.json page; used morgandev tools

--- express_server.js
+++ express_server.js
@@ -10,11 +10,17 @@ const urlDatabse = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+app.use(morganDev);
+
 
 app.get("/", (req, res) => {
   res.send("This is your only page!");
-})
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabse);
+});
 
 app.listen(PORT, () => {
-  console.log(`Listening on port: ${PORT}!`)
-})
+  console.log(`Listening on port: ${PORT}!`);
+});
