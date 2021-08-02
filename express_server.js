1684d315ce96e3be55a93cc282719ced7735e0f8
GledfullyChill <stephan.lofstrom@gmail.com>
2021-08-02 14:55:21 -0600

    first lines of code

---
+++ express_server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const morganDev = morgan('dev');
+const app = express();
+
+const PORT = 4321;
+
+const urlDatabse = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.send("This is your only page!");
+})
+
+app.listen(PORT, () = {
+  console.log(`Listening on port: ${PORT}!`)
+})
