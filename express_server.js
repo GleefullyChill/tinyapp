fbcd0249f7f3252682ae94f24eacb831cfdcbce8
GledfullyChill <stephan.lofstrom@gmail.com>
2021-08-03 18:24:56 -0600

    figured out how to access cookie and save the data

--- express_server.js
+++ express_server.js
@@ -1,11 +1,12 @@
+const { signedCookies } = require('cookie-parser');
+const cookieParser = require('cookie-parser');
 const express = require('express');
 const morgan = require('morgan');
 const morganDev = morgan('dev');
-
 const PORT = 4321;
-
 const app = express();
-
+app.use(morganDev);
+app.use(cookieParser());
 app.use(express.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
@@ -26,49 +27,10 @@ const generateRandomString = function() {
   }
   return str;
 };
-
 const urlDatabse = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-app.use(morganDev);
-
-app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabse };
-  res.render('urls_index', templateVars);
-});
-
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
-//after creating a new shortURL it redirects to the shorURL's respective /url
-app.post("/urls", (req, res) => {
-  const shortURL = generateRandomString();
-  urlDatabse[shortURL] = req.body.longURL;
-  res.redirect(`/urls/${shortURL}`);
-});
-app.post("/urls/:shortURL/Edit", (req, res) => {
-  const shortURL = req.params['shortURL'];
-  urlDatabse[shortURL] = req.body.longURL;
-  res.redirect(`/urls/${shortURL}`);
-});
-
-//redirects the shortURL from its respective /url to the corresponding web addreess
-app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabse[req.params.shortURL];
-  if (longURL === undefined) {
-    res.redirect('/urls');
-  } else {
-    res.redirect(longURL);
-  }
-});
-
-//should delete the respective shortURL
-app.post("/urls/:shortURL/delete", (req, res) => {
-  delete urlDatabse[req.params['shortURL']];
-  res.redirect(`/urls`);
-});
-
 app.get("/", (req, res) => {
   res.send("This is your HOME page!");
 });
@@ -81,10 +43,60 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+//a place to browse the short URLs and where they lead
+app.get("/urls", (req, res) => {
+  const templateVars = {
+    username: req.cookies["username"],
+    urls: urlDatabse
+  }
+  res.render('urls_index', templateVars);
+});
+//create a new short URL here
+app.get("/urls/new", (req, res) => {
+  const templateVars = {
+    username: req.cookies["username"]
+  }
+  res.render("urls_new", templateVars);
+});
+//redirects the shortURL from its respective /url to the corresponding web addreess
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabse[req.params.shortURL];
+  if (longURL === undefined) {
+    res.redirect('/urls');
+  } else {
+    res.redirect(longURL);
+  }
+});
+//where the details of the shorturls lives, including a way to edit where they go
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabse[req.params.shortURL]};//longURL/* What goes here? */ };
   res.render("urls_show", templateVars);
 });
+//after creating a new shortURL it redirects to the shorURL's respective /url
+app.post("/urls", (req, res) => {
+  const shortURL = generateRandomString();
+  urlDatabse[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
+});
+//edit where the short url links to from the short url's description page
+app.post("/urls/:shortURL/Edit", (req, res) => {
+  const shortURL = req.params['shortURL'];
+  urlDatabse[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
+});
+//should delete the respective shortURL
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabse[req.params['shortURL']];
+  res.redirect(`/urls`);
+});
+//a login POST
+app.post("/login", (req, res) => {
+  res.cookie('username', req.body['username'])
+  res.redirect('/urls')
+})
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}!`);
